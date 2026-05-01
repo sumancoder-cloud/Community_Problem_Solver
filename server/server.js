2407c2c13936aef769c46bf8816fe1a4a855be29
sumancoder-cloud <sumanyadav_tati@srmap.edu.in>
2026-05-02 01:24:24 +0530

    Remove invalid explicit OPTIONS route and rely on CORS middleware

--- server/server.js
+++ server/server.js
@@ -39,12 +39,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('/*', cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
